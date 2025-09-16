d8babd49d4f3166e524ba52a0a78c961fa471b43
IzharMohammed <izharmohammed21@gmail.com>
2025-09-16 19:20:20 +0530

    Feat:Initialized server

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,22 @@
-console.log("Hello world");
+import Fastify, { FastifyInstance } from 'fastify'
+
+const server: FastifyInstance = Fastify({
+    logger: true
+})
+
+server.get("/healthcheck", async function () {
+    return { status: "OK" }
+})
+
+async function main() {
+    try {
+        // await server.listen(3000, "0.0.0.0");
+        await server.listen({ port: 3000, host: "0.0.0.0" })
+        console.log(`Server ready at http://localhost:3000`);
+    } catch (e) {
+        console.error(e);
+        process.exit(1);
+    }
+}
+
+main();
